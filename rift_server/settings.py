2f55e09299f8a675c2b74692399413a49384408b
Mijael Viricochea <mijael.viricochea@outlook.com>
2022-10-11 23:22:41 -0400

    Add environment variables

--- rift_server/settings.py
+++ rift_server/settings.py
@@ -11,10 +11,14 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+import os
+
+environment_variables = Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -25,7 +29,7 @@ SECRET_KEY = 'django-insecure-z=ucf4(jn+y1*drc+6r)u@w)c%jmusa%ed8(3l7-vls1in*wd)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [environment_variables('ALLOWED_HOSTS')]
 
 
 # Application definition
